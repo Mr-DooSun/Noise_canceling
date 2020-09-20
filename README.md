f004b460c3ad90a81eb80c84888965d598c6ac86
Mr-DooSun <mrdoosun@gmail.com>
2020-09-20 12:28:45 +0900

    add file

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Noise_canceling
+# Python-Remove-Background-Noise
+
+
+### 필터의 효과
+
+![alt image](Figure_for_sample_1.png)
+
+▲위-sample_1.wav, 아래-필터를 거친 sample_1.wav
+![alt image](Figure_for_sample_2.png)
+
+▲위-sample_2.wav, 아래-필터를 거친 sample_2.wav
+
+직접 필터를 거친 샘플 청취를 해보면 드론의 소리는 대부분 사라지고
+사람의 목소리만 남아있다.
+
+### 필터의 원리
+사람의 음역대는 대략 300-3000Hz 이다. 
+그러나 다른 소음의 음역대와 겹치는 부분을 최대한 줄이기 위해 
+그 중 400-750Hz의 음역만 유지하고 나머지 부분은 소거하였다.
+
+그래서 시끄러운 환경에서도 상대적으로 조용한 사람의 목소리를 들을 수 있다.
+
+### 필터의 한계점
++ 사람의 음역대를 축소하였기 때문에 
+설정한 음역대를 초월하는 목소리를 낸다면 필터 과정에서 소거 될 수 있다.
++ 다른 사람이나 기계가 해당 음역대에 의도적으로 잡음을 만드는 것은 막을 수 없다. 
+
+### 사용법
+python 3.7을 설치하고 requirements.txt 에 있는 라이브러를 설치한다. \
+resource 에 필터를 거치고자하는 오디오 파일을 wav 로 입력후  main.py를 실행하면
+output 에 같은 이름으로 필터를 거친 오디오 파일이 생성된다. 
+
+
+### 참조
+- https://en.wikipedia.org/wiki/Voice_frequency
+- https://en.wikipedia.org/wiki/Low-pass_filter
+- https://en.wikipedia.org/wiki/High-pass_filter
---
+++ sample.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt  # 이미지 형태로 오디오의 파형을 그릴수 있게 해주는 라이브러리
+import numpy as np  # 수학과 관련된 기능을 고속으로 처리해주는 라이브러리
+import scipy.io.wavfile as wave  # WAV 포맷으로 입출력을 할 수 있게 해주는 라이브러리
+import speech_recognition as sr  # 음성인식과 관련된 유틸을 포함한 라이브러리
+from scipy import signal  # 신호처리와 관련된 유틸을 포함한 라이브러리
+
+
+# path : wav파일의 위치
+# show_plot : 필터링 전/후의 결과를 표시할지의 여부
+def filter(path, show_plot=False):
+    filename = path[path.rfind('/') + 1:]  # Wav파일의 이름을 구함
+    freq, raw_data = wave.read(path)  # Wav를 읽어서 데이터를 구함
+
+    data = []
+    if type(raw_data[0]) != np.int16:
+        for stereo in raw_data:
+            data.append(int(stereo[0]))
+    else:
+        data = raw_data
+    # 1~N개의 채널 중 첫번째 것만 추출함
+
+    b, a = signal.butter(5, 750 / (freq / 2), btype='highpass')
+    filtered = signal.lfilter(b, a, data)
+    # 750Hz 보다 위의 대역을 소거함
+
+    c, d = signal.butter(5, 450 / (freq / 2), btype='lowpass')
+    filtered = signal.lfilter(c, d, filtered)
+    # 400Hz 보다 아래의 대역을 소거함
+
+    filtered = np.int16(filtered / np.max(np.abs(filtered)) * 32767)
+    # 소리의 크기를 듣기 적절하게 조정함
+
+    if show_plot:  # 필터링/전후 결과 표시가 허용된 경우
+        fig, axs = plt.subplots(2, 1)  # 두개의 플롯을 가지는 창을 만듬
+        axs[0].plot(data)  # 첫번째 플롯에는 필터링 전의 데이터를 할당함
+        axs[1].plot(filtered)  # 두번째 플롯에는 필터링 후의 데이터를 할당함
+        plt.show()  # 창을 표시함
+
+    wave.write(f'output/{filename}', freq, filtered)  # Wav 포맷으로 파일을 출력함
+    return freq
+
+
+filename = ["resource/sample_1.wav","resource/sample_2.wav"]
+r = sr.Recognizer()
+# 파일이름, 음성인식기를 불러옴
+
+for i in range(0,2):
+	try:
+	    with sr.AudioFile(filename[i]) as source:  # 오디오 소스를 불러옴
+	        audio_data = r.record(source)  # 오디오 소스중 의미있는 부분인 오디오 데이터를 기록함
+	        open(filename[i], 'wb').write(audio_data.get_wav_data())  # 오디오 소스를 WAV포맷으로 파일시스템에 전송함
+	        freq = filter(filename[i])  # 필터링을 진행함
+
+	    with sr.AudioFile(filename[i]) as source:  # 필터링된 오디오 소스를 불러옴
+	        audio_data = r.record(source)  # 오디오 소스중 의미있는 부분인 오디오 데이터를 기록함
+	        text = r.recognize_google(audio_data, language='ko-KR')  # 오디오 데이터를 구글음성인식에 전달하여 평문으로 바꿈
+	        print(text)  # 텍스트를 콘솔에 출력함
+
+	        if "도와" in text:  # 평문에 해당 글자가 포함되면 파일시스템에 전송함
+	            open(f'저장된 호출/sample_{i+1}.wav', 'wb').write(audio_data.get_wav_data())
+	            i += 1
+	        else:  # 그렇지 않다면 이상없음을 콘솔에 출력함
+	            print("이상없음")
+	except sr.UnknownValueError as e:
+	    print("음성을 인식할수없습니다.", e)  # 음성인식에 실패하면 콘솔에 출력함
